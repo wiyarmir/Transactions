e0891f535bb1dbdf41583fc4a9f1c972fc45b14a
Guillermo Orellana <wiyarmir@gmail.com>
2015-11-21 19:33:38 +0000

    if one jump is not enough, keep jumping until you visited all nodes

--- app/src/main/java/es/guillermoorellana/transactions/RateConverter.java
+++ app/src/main/java/es/guillermoorellana/transactions/RateConverter.java
@@ -4,6 +4,16 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Converts rates.
+ * <p/>
+ * Stores conversion rates in a tree emulated by using two maps. Initially only populates it with
+ * the provided input data, and lazily populates the rest as needed.
+ * <p/>
+ * Sacrificed readability and performance. Would improve readability using a dedicated weighed tree
+ * class which would make method calls more meaningful. Would improve performance using arraymaps
+ * and making an initial estimation of the array sizes needed so there would not be that many allocs
+ */
 public class RateConverter {
     Map<String, Map<String, Float>> conversionTree;
 
@@ -30,13 +40,17 @@ public class RateConverter {
         if (fromRates.containsKey(to)) {
             return fromRates.get(to) * amount;
         } else {
-            for (Map.Entry<String, Map<String, Float>> entry : conversionTree.entrySet()) {
-                if (fromRates.containsKey(entry.getKey())
-                        && entry.getValue().containsKey(to)) {
-                    float combinedRate = entry.getValue().get(to) * fromRates.get(entry.getKey());
-                    fromRates.put(to, combinedRate);
-                    return amount * combinedRate;
+            int attempts = 0;
+            while (attempts < conversionTree.size()) {
+                for (Map.Entry<String, Map<String, Float>> entry : conversionTree.entrySet()) {
+                    if (fromRates.containsKey(entry.getKey())
+                            && entry.getValue().containsKey(to)) {
+                        float combinedRate = entry.getValue().get(to) * fromRates.get(entry.getKey());
+                        fromRates.put(to, combinedRate);
+                        return amount * combinedRate;
+                    }
                 }
+                attempts++;
             }
         }
         throw new UnsupportedOperationException();
