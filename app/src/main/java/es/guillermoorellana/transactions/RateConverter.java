dbb0ab2122ee8eb7b2203c7703fbb7028dedae0c
Guillermo Orellana <wiyarmir@gmail.com>
2015-11-21 19:32:39 +0000

    search in neighbours

--- app/src/main/java/es/guillermoorellana/transactions/RateConverter.java
+++ app/src/main/java/es/guillermoorellana/transactions/RateConverter.java
@@ -29,6 +29,15 @@ public class RateConverter {
         Map<String, Float> fromRates = conversionTree.get(from);
         if (fromRates.containsKey(to)) {
             return fromRates.get(to) * amount;
+        } else {
+            for (Map.Entry<String, Map<String, Float>> entry : conversionTree.entrySet()) {
+                if (fromRates.containsKey(entry.getKey())
+                        && entry.getValue().containsKey(to)) {
+                    float combinedRate = entry.getValue().get(to) * fromRates.get(entry.getKey());
+                    fromRates.put(to, combinedRate);
+                    return amount * combinedRate;
+                }
+            }
         }
         throw new UnsupportedOperationException();
     }
