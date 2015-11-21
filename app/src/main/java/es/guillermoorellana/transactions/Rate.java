21d24538c1cbbc86f56c413fe7779627935ebbba
Guillermo Orellana <wiyarmir@gmail.com>
2015-11-21 19:28:57 +0000

    test entity hydration

---
+++ app/src/main/java/es/guillermoorellana/transactions/Rate.java
@@ -0,0 +1,7 @@
+package es.guillermoorellana.transactions;
+
+public class Rate {
+    public String from;
+    public String to;
+    public float rate;
+}
---
+++ app/src/test/java/es/guillermoorellana/transactions/RatesTest.java
@@ -0,0 +1,31 @@
+package es.guillermoorellana.transactions;
+
+import com.google.gson.Gson;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class RatesTest {
+
+    private final String json = "[{\"from\":\"USD\",\"rate\":\"0.77\",\"to\":\"GBP\"},{\"from\":\"GBP\",\"rate\":\"1.3\",\"to\":\"USD\"},{\"from\":\"USD\",\"rate\":\"1.09\",\"to\":\"CAD\"},{\"from\":\"CAD\",\"rate\":\"0.92\",\"to\":\"USD\"},{\"from\":\"GBP\",\"rate\":\"0.83\",\"to\":\"AUD\"},{\"from\":\"AUD\",\"rate\":\"1.2\",\"to\":\"GBP\"}]";
+    private Gson gson;
+
+    @Before
+    public void setUp() throws Exception {
+        gson = new Gson();
+    }
+
+    @Test
+    public void testRatesAreRead() {
+        getRateList();
+    }
+
+    private List<Rate> getRateList() {
+        Rate[] array = gson.fromJson(json, Rate[].class);
+        return Arrays.asList(array);
+    }
+
+}
