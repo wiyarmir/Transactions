2fd62f9fea9e971937725b994e495f90a2c22d5e
Guillermo Orellana <wiyarmir@gmail.com>
2015-11-21 19:31:52 +0000

    easy case

--- app/src/main/java/es/guillermoorellana/transactions/RateConverter.java
+++ app/src/main/java/es/guillermoorellana/transactions/RateConverter.java
@@ -1,14 +1,35 @@
 package es.guillermoorellana.transactions;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class RateConverter {
+    Map<String, Map<String, Float>> conversionTree;
 
     public RateConverter(List<Rate> rateList) {
-
+        conversionTree = new HashMap<>();
+        // initial population of the tree
+        for (Rate rate : rateList) {
+            Map<String, Float> leaf;
+            if (!conversionTree.containsKey(rate.from)) {
+                leaf = new HashMap<>();
+                conversionTree.put(rate.from, leaf);
+            } else {
+                leaf = conversionTree.get(rate.from);
+            }
+            leaf.put(rate.to, rate.rate);
+        }
     }
 
     public float convert(String from, String to, float amount) {
-        return 0;
+        if (!conversionTree.containsKey(from)) {
+            throw new UnsupportedOperationException();
+        }
+        Map<String, Float> fromRates = conversionTree.get(from);
+        if (fromRates.containsKey(to)) {
+            return fromRates.get(to) * amount;
+        }
+        throw new UnsupportedOperationException();
     }
 }
