18a7e3e33df911bc7e0f43fc4cdc233246504bc9
Guillermo Orellana <wiyarmir@gmail.com>
2015-11-22 01:42:48 +0000

    optimisations

--- app/src/main/java/es/guillermoorellana/transactions/RateConverter.java
+++ app/src/main/java/es/guillermoorellana/transactions/RateConverter.java
@@ -1,7 +1,7 @@
 package es.guillermoorellana.transactions;
 
-import android.support.v4.util.ArrayMap;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -9,25 +9,22 @@ import java.util.Map;
  * Converts rates.
  * <p/>
  * Stores conversion rates in a tree emulated by using two maps. Initially only populates it with
- * the provided input data, and lazily populates the rest as needed. It does not attempt to recognise
- * if a node has already been visited nor attempt to use inverse conversions.
+ * the provided input data, and lazily populates the rest as needed.
  * <p/>
  * Sacrificed readability and performance. Would improve readability using a dedicated weighed tree
  * class (or weighed graph) which would make method calls more meaningful. Would improve performance
- * using arraymaps and making an initial estimation of the array sizes needed so there would not be
- * that many allocs Would make it a bit more intelligent by keeping already visited nodes not to
- * revisit them and use invert conversions (1/known conversion).
+ * not using HashMaps but ArrayMaps (gives StackOverflow, must be some impl. detail of ArrayMap).
  */
 public class RateConverter {
     Map<String, Map<String, Float>> conversionTree;
 
     public RateConverter(List<Rate> rateList) {
-        conversionTree = new ArrayMap<>(rateList.size() / 2);
+        conversionTree = new HashMap<>(rateList.size() / 2);
         // initial population of the tree
         for (Rate rate : rateList) {
             Map<String, Float> leaf;
             if (!conversionTree.containsKey(rate.from)) {
-                leaf = new ArrayMap<>();
+                leaf = new HashMap<>();
                 conversionTree.put(rate.from, leaf);
             } else {
                 leaf = conversionTree.get(rate.from);
@@ -37,32 +34,37 @@ public class RateConverter {
     }
 
     public float convert(String from, String to, float amount) {
+        return amount * conversionRate(from, to);
+    }
+
+    private float conversionRate(String from, String to) {
         if (from.equals(to)) { // no conversion required
-            return amount;
+            return 1;
         }
         if (!conversionTree.containsKey(from)) { // starting currency unknown
             throw new UnsupportedOperationException();
         }
-        Map<String, Float> knownRates = conversionTree.get(from);
-        if (knownRates.containsKey(to)) { // target currency is in our known map
-            return knownRates.get(to) * amount;
+        Map<String, Float> fromRates = conversionTree.get(from);
+        if (fromRates.containsKey(to)) { // target currency is in our known map
+            return fromRates.get(to);
         } else { // unknown path to target currency, must search
-            int attempts = 0;
-            while (attempts < conversionTree.size()) {
-                for (Map.Entry<String, Map<String, Float>> entry : conversionTree.entrySet()) {
-                    if (entry.getKey().equals(from)) {
-                        continue;
-                    }
-                    if (knownRates.containsKey(entry.getKey())
-                            && entry.getValue().containsKey(to)) {
-                        float combinedRate = entry.getValue().get(to) * knownRates.get(entry.getKey());
-                        knownRates.put(to, combinedRate);
-                        return amount * combinedRate;
+            for (Map.Entry<String, Map<String, Float>> midEntry : conversionTree.entrySet()) {
+                String midCurrency = midEntry.getKey();
+                Map<String, Float> midRates = midEntry.getValue();
+                if (midCurrency.equals(from)) {// skip ourselves
+                    continue;
+                }
+                if (fromRates.containsKey(midCurrency)) { // we know how to get there
+                    if (!midRates.containsKey(to)) { // but we are not there yet
+                        return fromRates.get(midCurrency) * conversionRate(midCurrency, to); // keep looking
+                    } else { // found!
+                        float midRate = fromRates.get(midCurrency) * midRates.get(to);
+                        fromRates.put(midCurrency, midRate); // cache search
+                        return midRate;
                     }
                 }
-                attempts++;
             }
         }
-        throw new UnsupportedOperationException();
+        throw new UnsupportedOperationException(); // we tried!
     }
 }
--- app/src/test/java/es/guillermoorellana/transactions/RatesTest.java
+++ app/src/test/java/es/guillermoorellana/transactions/RatesTest.java
@@ -7,11 +7,23 @@ import static org.junit.Assert.assertThat;
 
 public class RatesTest {
     @Test
-    public void testConversion() {
+    public void testConversionSelf() {
         RateConverter converter = new RateConverter(TestDataRepository.getRateList());
-        assertThat(converter.convert("USD", "USD", 1), is(equalTo(1.0f))); // own
-        assertThat(converter.convert("USD", "GBP", 1), is(equalTo(0.77f))); // direct
-        assertThat(converter.convert("CAD", "GBP", 1), is(equalTo(0.7084f))); // one jump
-        assertThat(converter.convert("CAD", "AUD", 1), is(equalTo(0.587972f))); // two jumps
+        assertThat(converter.convert("USD", "USD", 1), is(equalTo(1.0f)));
+    }
+    @Test
+    public void testConversionDirect() {
+        RateConverter converter = new RateConverter(TestDataRepository.getRateList());
+        assertThat(converter.convert("USD", "GBP", 1), is(equalTo(0.77f)));
+    }
+    @Test
+    public void testConversionJumpOne() {
+        RateConverter converter = new RateConverter(TestDataRepository.getRateList());
+        assertThat(converter.convert("CAD", "GBP", 1), is(equalTo(0.7084f)));
+    }
+    @Test
+    public void testConversionJumpTwo() {
+        RateConverter converter = new RateConverter(TestDataRepository.getRateList());
+        assertThat(converter.convert("CAD", "AUD", 1), is(equalTo(0.587972f)));
     }
 }
